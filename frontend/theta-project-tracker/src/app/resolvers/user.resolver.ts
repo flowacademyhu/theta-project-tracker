a0aa5cb2b00709056b80a42837406dbe64fde1c7
petarda33 <sztantics.peter93@gmail.com>
2020-06-16 13:46:35 +0200

    creeate user interface, CRUD, user model

---
+++ frontend/theta-project-tracker/src/app/resolvers/user.resolver.ts
@@ -0,0 +1,23 @@
+
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { User } from '../models/user.model'
+import { UserService } from '../user.service';
+
+@Injectable({ providedIn: 'root' })
+export class UserResolver implements Resolve<User[]> {
+  constructor(private UserService: UserService) {}
+
+  public resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<User[]> | Promise<User[]> | User[] {
+    const id = +route.params.id;
+    return this.UserService.fetchUsers();
+  }
+}
---
+++ frontend/theta-project-tracker/src/app/services/user.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { Observable, asapScheduler, BehaviorSubject } from 'rxjs';
+import { User, Role } from '../models/user.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor() { }
+
+  public users: User[] = [{
+    id: 1,
+    firstName: 'Bence',
+    lastName: 'Rácz',
+    role: Role.ADMIN,
+    email: 'asd@asd.com',
+    password: 'asdasd',
+    userCostToCompanyPerHour: 50,
+    projectAssigned: {projectName: 'xy', userCostPerHour: 50}
+  },
+  {
+    id: 2,
+    firstName: 'Máté',
+    lastName: 'Szabó',
+    role: Role.USER,
+    email: 'asdasd@asd.com',
+    password: 'asdasdasd',
+    userCostToCompanyPerHour: 70,
+    projectAssigned: {projectName: 'xyz', userCostPerHour: 70}
+  }
+  ]
+  users$: BehaviorSubject<User[]> = new BehaviorSubject<User[]>(this.users)
+  
+  public fetchUsers (): User[] {
+    return this.users
+  }
+
+  public fetchUser(id: number) {
+    return { ...this.users.find(user => user.id === id) };
+  }
+
+  public addUser(user: User) {
+    user.id = this.users.length + 1;
+    this.users.push(user);
+  }
+
+  public updateUser(id: number, user: User) {
+    const index = this.users.findIndex(char => char.id === id);
+    this.users[index] = user;
+  }
+
+  public deleteUser(id: number): Observable<User> {
+    return this.http.delete<>(`/$(id)`)
+  }
+
+}
+
+
+
+
