13bd5bf2641ca33ef3c3558d2c7253e90be040e0
Skatole <r.bencelaszlo9511@gmail.com>
2020-06-16 13:49:35 +0200

    Master created

---
+++ ProjectVoodoo/src/app/components/header.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" routerLink="/">Heroes & Villains</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li routerLinkActive="active">
+            <a routerLink="characters">Timesheet</a>
+          </li>
+          <li routerLinkActive="active">
+            <a [routerLink]="['characters', 'new']">Users</a>
+          </li>
+          <li routerLinkActive="active">
+            <a [routerLink]="['characters', 'new']">Reports</a>
+          </li>
+          <li routerLinkActive="active">
+            <a [routerLink]="['characters', 'new']">Calendar</a>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a (click)="onLogout()">Logged in as: Test Elek (LOGOUT)</a></li>
+        </ul>
+      </div>
+    </nav>
+  `
+})
+export class HeaderComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {}
+
+  public onLogout() {
+    this.authService.logout();
+    this.router.navigate(['login']);
+  }
+}
