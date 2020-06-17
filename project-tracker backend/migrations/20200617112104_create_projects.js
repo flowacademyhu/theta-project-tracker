bf441125eb2f6a5e60f0280b106c0121ad258689
viktor <szabomatedenes@gmail.com>
2020-06-17 12:38:07 +0200

    I19 project crud

---
+++ project-tracker backend/migrations/20200617112104_create_projects.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('projects', (table) => {
+        table.increments();
+        table.string('name');
+        table.integer('clientId').unsigned().references('clients.id');;
+        table.string('description');
+        table.integer('budget');
+        table.timestamp('updatedAt').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        table.timestamp('createdAt').defaultTo(knex.raw('CURRENT_TIMESTAMP'));
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('projects');
+};
