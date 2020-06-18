c220acae0033354b616a319274c339c51acbe60e
Youngcatlady95 <szizitaa@gmail.com>
2020-06-18 09:28:52 +0200

    every working import we need so far

--- frontend/theta-project-tracker/src/app/app.module.ts
+++ frontend/theta-project-tracker/src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewUserModalComponent } from '../app/modals/new-user-modal.component';
+import { DeleteModalComponent } from '../app/modals/delete-modal.component';
+import { NewUserComponent } from '../app/components/new-user.component';
+import { UsersComponent } from '../app/components/users.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserModalComponent,
+    NewUserComponent,
+    UsersComponent,
+    DeleteModalComponent,
+    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatTableModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
