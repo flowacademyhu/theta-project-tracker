f411d03d27e1ca76b09f485fbaeee47e1d1c50f5
petarda33 <sztantics.peter93@gmail.com>
2020-06-18 09:49:38 +0200

    internationalize modals

--- frontend/theta-project-tracker/src/app/modals/new-user-modal.component.ts
+++ frontend/theta-project-tracker/src/app/modals/new-user-modal.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-user-modal',
   template: `
-  <h2 mat-dialog-title><strong>Add New User</strong></h2>
+  <h2 mat-dialog-title><strong>{{'Add user' | translate}}</strong></h2>
   <mat-dialog-content class="mat-typography">
     <app-new-user></app-new-user>
   </mat-dialog-content>
