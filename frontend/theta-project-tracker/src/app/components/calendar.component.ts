789f48e1f7e92d931c4926177429f170d98c2877
MasaT91 <tamas.szemesi@gmail.com>
2020-06-18 11:45:43 +0200

    generated components

---
+++ frontend/theta-project-tracker/src/app/components/calendar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calender',
+  template: ``,
+  styles: [``]
+})
+export class CalendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/theta-project-tracker/src/app/components/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: ``,
+  styles: [``]
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
