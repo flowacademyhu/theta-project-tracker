16567cd307e08b6572e1e517796644ef0bf92c11
viktor <szabomatedenes@gmail.com>
2020-06-18 14:28:42 +0200

    deleted comments

--- project-tracker backend/src/app/routers/projectUser.ts
+++ project-tracker backend/src/app/routers/projectUser.ts
@@ -6,5 +6,3 @@ export const router: Router = Router({ mergeParams: true });
 router.get('/', projectUserController.index);
 router.post('/:userId', projectUserController.create);
 router.delete('/:userId', projectUserController.destroy);
-/*router.get('/:id', projectUserController.show);
-router.put('/:id', projectUserController.update);*/
--- project-tracker backend/src/app/routers/userProject.ts
+++ project-tracker backend/src/app/routers/userProject.ts
@@ -4,7 +4,3 @@ import * as userProjectController from '../controllers/userProject';
 export const router: Router = Router({ mergeParams: true });
 
 router.get('/', userProjectController.index);
-/*router.post('/:userId', projectUserController.create);
-router.delete('/:userId', projectUserController.destroy);
-router.get('/:id', projectUserController.show);
-router.put('/:id', projectUserController.update);*/
