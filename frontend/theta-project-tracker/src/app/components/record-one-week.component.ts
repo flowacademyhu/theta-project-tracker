3544b753240e932a979112d773fc2a7af177b645
Youngcatlady95 <szizitaa@gmail.com>
2020-06-23 18:34:04 +0200

    IT'S WORKING! i know, it's ugly, but i spent way too much time with it

--- frontend/theta-project-tracker/src/app/components/record-one-week.component.ts
+++ frontend/theta-project-tracker/src/app/components/record-one-week.component.ts
@@ -8,54 +8,36 @@ import { EventEmitter } from '@angular/core';
   template: `
   <div class="wrapper">
   <mat-grid-list cols="30" rowHeight="100px">
-  <mat-grid-tile
-  class="tile"
-  [colspan]="3"
-  [rowspan]="1"><strong>{{ project.projectName }}</strong>
-
-</mat-grid-tile>
-<mat-grid-tile
-class="tile"
-[colspan]="3"
-[rowspan]="1" >
-project.milestone
-</mat-grid-tile>
-<mat-grid-tile
-class="tile"
-[colspan]="1"
-[rowspan]="1"
->
-<mat-icon>sms</mat-icon>
-</mat-grid-tile>
-  <mat-grid-tile
-  class="tile"
-      *ngFor="let day of days"
-      [colspan]="day.cols"
-      [rowspan]="day.rows">
+    <mat-grid-tile class="tile" [colspan]="3" [rowspan]="1"><strong>{{ project.projectName }}</strong>
+    </mat-grid-tile>
+    <mat-grid-tile class="tile" [colspan]="3" [rowspan]="1">
+      project.milestone
+    </mat-grid-tile>
+    <mat-grid-tile class="tile" [colspan]="1" [rowspan]="1">
+      <mat-icon>sms</mat-icon>
+    </mat-grid-tile>
+    <mat-grid-tile class="tile" *ngFor="let day of days" [colspan]="day.cols" [rowspan]="day.rows">
       <form [formGroup]="timeSheet">
-      <mat-form-field appearance="outline" formGroupName="normalHours">
-      <input type="number" matInput formControlName="{{ day.name }}">
-    </mat-form-field>
-    <mat-form-field appearance="outline" color="warn" formGroupName="overTime">
-      <input type="number" matInput placeholder="OT" formControlName="{{ day.name }}">
-    </mat-form-field>
-    </form>
-  </mat-grid-tile>
-  <mat-grid-tile
-  class="tile"
-[colspan]="2"
-[rowspan]="1">
-<button mat-icon-button *ngIf="isEditVisible">
-<mat-icon (click)="editRecords()">edit</mat-icon>
-</button>
-        <button mat-icon-button>
-          <mat-icon (click)="onDeleteProject()">clear</mat-icon>
-        </button>
-        <button mat-icon-button (click)="getDailyHours()" *ngIf="!isEditVisible">
-          <mat-icon>save</mat-icon>
-        </button>
-</mat-grid-tile>
-</mat-grid-list>
+        <mat-form-field appearance="outline" formGroupName="normalHours">
+          <input type="number" matInput formControlName="{{ day.name }}">
+        </mat-form-field>
+        <mat-form-field appearance="outline" color="warn" formGroupName="overTime">
+          <input type="number" matInput placeholder="OT" formControlName="{{ day.name }}">
+        </mat-form-field>
+      </form>
+    </mat-grid-tile>
+    <mat-grid-tile class="tile" [colspan]="2" [rowspan]="1">
+      <button mat-icon-button *ngIf="isEditVisible">
+        <mat-icon (click)="editRecords()">edit</mat-icon>
+      </button>
+      <button mat-icon-button>
+        <mat-icon (click)="onDeleteProject()">clear</mat-icon>
+      </button>
+      <button mat-icon-button (click)="getDailyHours()" *ngIf="!isEditVisible">
+        <mat-icon>save</mat-icon>
+      </button>
+    </mat-grid-tile>
+  </mat-grid-list>
 </div>
   `,
   styles: [`
@@ -87,8 +69,6 @@ export class RecordOneWeekComponent implements OnInit {
   ];
   @Output() projectEmitter: EventEmitter<Object> = new EventEmitter<Object>();
   @Output() projectToDelete: EventEmitter<string> = new EventEmitter<string>();
-  @Output() projectToEdit: EventEmitter<string> = new EventEmitter<string>();
-  @Output() cicaEmitter: EventEmitter<number> = new EventEmitter<number>();
   isEditVisible: boolean;
   timeSheet = new FormGroup({
     normalHours: new FormGroup({
@@ -110,22 +90,9 @@ export class RecordOneWeekComponent implements OnInit {
       sunday: new FormControl(),
     })
   })
-/*   projectTotal = {
-    name: this.project.projectName,
-    normalHours: this.timeSheet.get('normalHours'),
-    overTime: this.timeSheet.get('overTime')
-  } */
   constructor() { }
   ngOnInit(): void {
-  
   }
-  editRecords() {
-    this.timeSheet.enable();
-    this.projectToEdit.emit(this.project.projectName)
-  }
-
- 
-
   getDailyHours() {
     let projectTotal = {
       name: this.project.projectName,
@@ -133,13 +100,7 @@ export class RecordOneWeekComponent implements OnInit {
       overTime: this.timeSheet.get('overTime').value
     }
     this.projectEmitter.emit(projectTotal);
- /*    this.timeSheet.valueChanges.pipe(
-      distinctUntilChanged((v1, v2) => v1 !== v2)
-    ).subscribe(value => {
-      this.projectEmitter.emit(this.projectTotal);
-    }) */
   }
-
   onDeleteProject() {
     this.projectToDelete.emit(this.project.projectName);
   }
--- frontend/theta-project-tracker/src/app/components/timesheet.component.ts
+++ frontend/theta-project-tracker/src/app/components/timesheet.component.ts
@@ -7,20 +7,20 @@ import { ProjectAssigned } from '../models/user.model';
   template: `
   <div>
   <ng-container *ngFor="let project of projects; let i = index">
-  <app-record-one-week [project]="project"
-  (projectEmitter)="recordDailyHours($event)"
-  (projectToDelete)="destroyProject($event)"
-  ></app-record-one-week>
-  <mat-divider></mat-divider>
+    <app-record-one-week [project]="project" (projectEmitter)="recordDailyHours($event)"
+      (projectToDelete)="destroyProject($event)"></app-record-one-week>
+    <mat-divider></mat-divider>
   </ng-container>
   <div class="footer">
-  Total 
-  <mat-grid-list cols="7" rowHeight="30px">
-  <mat-grid-tile *ngFor="let day of days" [colspan]="1" ><p>{{ day.total }}&emsp;</p><p [ngStyle]="{'color': 'red'}">{{ day.overTime }}</p></mat-grid-tile>
-  </mat-grid-list>
-  
+    Total
+    <mat-grid-list cols="7" rowHeight="30px">
+      <mat-grid-tile *ngFor="let day of days" [colspan]="1">
+        <p>{{ day.total }}&emsp;</p>
+        <p [ngStyle]="{'color': 'red'}">{{ day.overTime }}</p>
+      </mat-grid-tile>
+    </mat-grid-list>
   </div>
-  </div>
+</div>
   `,
   styles: [`
   div{
@@ -65,70 +65,101 @@ export class TimesheetComponent implements OnInit {
       sunday: number;
     }
   }[] = [];
-  monday = 0;
-  tuesday = 0;
-  wednesday = 0;
-  thursday = 0;
-  friday = 0;
-  saturday = 0;
-  sunday = 0;
-  mondayOver = 0;
-  tuesdayOver = 0;
-  wednesdayOver = 0;
-  thursdayOver = 0;
-  fridayOver = 0;
-  saturdayOver = 0;
-  sundayOver = 0;
-  total = 0;
-  days = [
-    { name: 'monday', total: this.monday, overTime: this.mondayOver },
-    { name: 'tuesday', total: this.tuesday, overTime: this.tuesdayOver },
-    { name: 'wednesday', total: this.wednesday, overTime: this.wednesdayOver },
-    { name: 'thursday', total: this.thursday, overTime: this.thursdayOver },
-    { name: 'friday', total: this.friday, overTime: this.fridayOver },
-    { name: 'saturday', total: this.saturday, overTime: this.saturdayOver },
-    { name: 'sunday', total: this.sunday, overTime: this.sundayOver },
-  ]
+  days: {name:string, total: number, overTime: number}[] = [];
   constructor(private authService: AuthService) { }
-
-
   ngOnInit(): void {
     this.projects = this.authService.getAdmin().projectAssigned;
   }
   recordDailyHours(event: any) {
-    console.log(event.name);
-    if (this.projectsArrived.length === 0) {
-      this.projectsArrived.push(event)
-    } else if (this.projectsArrived.length > 0) {
-      this.projectsArrived.push(event);
-      for (let i = 0; i < this.projectsArrived.length; i++) {
-        if (this.projectsArrived[i].name === event.name) {
-          this.projectsArrived[i] = event;
-        }
-      }
-    }
-
-    console.log(this.projectsArrived)
-    this.getMondayTotals()
+   if (this.projectsArrived.length === 0) {
+     this.projectsArrived.push(event)
+   } else {
+     for(let i = 0; i < this.projectsArrived.length; i++) {
+       if (this.projectsArrived[i].name === event.name) {
+         this.projectsArrived[i] = event
+         this.getWeeklyTotal()
+         return this.projectsArrived
+       }
+     }
+     this.projectsArrived.push(event)
+   }
+    this.getWeeklyTotal()
+  }
+  getWeeklyTotal() {
+    this.getMondayTotals();
+    this.getTuesdayTotals();
+    this.getWednesdayTotals();
+    this.getThursdayTotals();
+    this.getFridayTotals();
+    this.getSaturdayTotals();
+    this.getSundayTotals();
   }
   getMondayTotals() {
     let total = 0;
+    let over = 0;
     for (let i = 0; i < this.projectsArrived.length; i++) {
       total += this.projectsArrived[i].normalHours.monday
+      over += this.projectsArrived[i].overTime.monday
     }
-    this.monday = total;
-    this.days = [
-      { name: 'monday', total: this.monday, overTime: this.mondayOver },
-      { name: 'tuesday', total: this.tuesday, overTime: this.tuesdayOver },
-      { name: 'wednesday', total: this.wednesday, overTime: this.wednesdayOver },
-      { name: 'thursday', total: this.thursday, overTime: this.thursdayOver },
-      { name: 'friday', total: this.friday, overTime: this.fridayOver },
-      { name: 'saturday', total: this.saturday, overTime: this.saturdayOver },
-      { name: 'sunday', total: this.sunday, overTime: this.sundayOver },
-    ]
+    return this.days[0] = {name: 'monday', total: total, overTime: over};
+  }
+  getTuesdayTotals() {
+    let total = 0;
+    let over = 0;
+    for (let i = 0; i < this.projectsArrived.length; i++) {
+      total += this.projectsArrived[i].normalHours.tuesday
+      over += this.projectsArrived[i].overTime.tuesday
+    }
+    return this.days[1] = {name: 'tuesday', total: total, overTime: over};
+  }
+  getWednesdayTotals() {
+    let total = 0;
+    let over = 0;
+    for (let i = 0; i < this.projectsArrived.length; i++) {
+      total += this.projectsArrived[i].normalHours.wednesday;
+      over += this.projectsArrived[i].overTime.wednesday
+    }
+    return this.days[2] = {name: 'wednesday', total: total, overTime: over};
+  }
+  getThursdayTotals() {
+    let total = 0;
+    let over = 0;
+    for (let i = 0; i < this.projectsArrived.length; i++) {
+      total += this.projectsArrived[i].normalHours.thursday;
+      over += this.projectsArrived[i].overTime.thursday
+    }
+    return this.days[3] = {name: 'thursday', total: total, overTime: over};
+  }
+  getFridayTotals() {
+    let total = 0;
+    let over = 0;
+    for (let i = 0; i < this.projectsArrived.length; i++) {
+      total += this.projectsArrived[i].normalHours.friday;
+      over += this.projectsArrived[i].overTime.friday
+    }
+    return this.days[4] = {name: 'friday', total: total, overTime: over};
+  }
+  getSaturdayTotals() {
+    let total = 0;
+    let over = 0;
+    for (let i = 0; i < this.projectsArrived.length; i++) {
+      total += this.projectsArrived[i].normalHours.saturday;
+      over += this.projectsArrived[i].overTime.saturday
+    }
+    return this.days[5] = {name: 'saturday', total: total, overTime: over};
+  }
+  getSundayTotals() {
+    let total = 0;
+    let over = 0;
+    for (let i = 0; i < this.projectsArrived.length; i++) {
+      total += this.projectsArrived[i].normalHours.sunday;
+      over += this.projectsArrived[i].overTime.sunday
+    }
+    return this.days[6] = {name: 'sunday', total: total, overTime: over};
   }
   destroyProject(event: any) {
     this.projects.splice(this.projects.findIndex(p => p.projectName === event), 1);
+    this.projectsArrived.splice(this.projects.findIndex(p => p.projectName === event), 1);
+    this.getWeeklyTotal();
   }
-
 }
