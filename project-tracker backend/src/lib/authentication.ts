a2e41b53fa83a88fd437a4b6f09865cb7ac55727
JJazon125 <jazon97@gmail.com>
2020-06-24 13:46:23 +0200

    refact spaces (one file)

--- project-tracker backend/src/lib/authentication.ts
+++ project-tracker backend/src/lib/authentication.ts
@@ -5,47 +5,47 @@ import * as jwtConfig from '../../config/jwt.json'
 import {User} from "../app/models/user";
 
 enum Method {
-    get = 'GET',
-    post = 'POST',
-    put = 'PUT',
-    destroy = 'DELETE'
+  get = 'GET',
+  post = 'POST',
+  put = 'PUT',
+  destroy = 'DELETE'
 }
 
 interface Endpoint {
-    path: string;
-    method: Method;
+  path: string;
+  method: Method;
 }
 
 const anonymusEndpoints: Array<Endpoint> = [
-    {
-        path: '/login',
-        method: Method.post
-    }
+  {
+    path: '/login',
+    method: Method.post
+  }
 ];
 
 const isAnonymusEndpoint = (req: Request): boolean => {
-    return !!(anonymusEndpoints.find(
-        anonymusEndpoint => (
-            anonymusEndpoint.method === req.method && anonymusEndpoint.path === req.path
-        )))
+  return !!(anonymusEndpoints.find(
+    anonymusEndpoint => (
+      anonymusEndpoint.method === req.method && anonymusEndpoint.path === req.path
+    )))
 }
 
 const verifyUser = async (req: Request, res: Response) => {
-    const token: string = req.headers.authorization.split(' ')[1];
-    const info = jwt.verify(token, jwtConfig.secret);
-    const userId: number = info.userId;
-    const user: User = await database('users').where({id: userId}).first();
-    res.locals.user = user;
+  const token: string = req.headers.authorization.split(' ')[1];
+  const info = jwt.verify(token, jwtConfig.secret);
+  const userId: number = info.userId;
+  const user: User = await database('users').where({id: userId}).first();
+  res.locals.user = user;
 }
 
 export const authentication = async (req: Request, res: Response, next: NextFunction) => {
-    if (isAnonymusEndpoint(req)) {
-        return next();
-    }
-    try {
-        await verifyUser(req, res);
-        next();
-    } catch (error) {
-        res.sendStatus(401);
-    }
+  if (isAnonymusEndpoint(req)) {
+    return next();
+  }
+  try {
+    await verifyUser(req, res);
+    next();
+  } catch (error) {
+    res.sendStatus(401);
+  }
 }
