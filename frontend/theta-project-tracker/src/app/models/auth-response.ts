1c8930a64460234c8fdd8f9eb4565ff28f5bd016
MasaT91 <tamas.szemesi@gmail.com>
2020-06-26 14:46:12 +0200

    refactored auth response

--- frontend/theta-project-tracker/src/app/models/auth-response.ts
+++ frontend/theta-project-tracker/src/app/models/auth-response.ts
@@ -1,13 +1,3 @@
 export interface AuthResponse {
   token: string;
-  user: {
-    id: number,
-    firstName: string,
-    lastName: string,
-    role: string,
-    email: string,
-    costToCompanyPerHour: number,
-    updatedAt: string,
-    createdAt: string
-  };
 }
