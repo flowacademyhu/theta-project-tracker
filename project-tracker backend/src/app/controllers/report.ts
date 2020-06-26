3424d6051997b02a70c60d555a2a0acaa32c1ff4
viktor <szabomatedenes@gmail.com>
2020-06-26 07:08:03 +0200

    generic report generator works for 4 reports, possibly needs refactoring

--- project-tracker backend/src/app/controllers/report.ts
+++ project-tracker backend/src/app/controllers/report.ts
@@ -12,10 +12,51 @@ export const generateReportProjectByHours = async (req: Request, res: Response) 
         .groupBy('users.id', 'projects.id', 'projectName', 'userName')
         .sum('timeRecords.spentTime as timeSpent');
     const report = await query;
-    res.json(parseReportForFrontendGeneric(report, 'userName', 'projectName', 'timeSpent'));
+    res.json(transformReportForFrontendGeneric(report, 'userName', 'projectName', 'timeSpent'));
 }
-export const parseReportForFrontendGeneric = (report: Array<object>, columnName: string, rowName: string, data: string): object => {
-    let columnNames = getUniqueColumnNames(report);
+export const generateReportProjectByPounds = async (req: Request, res: Response) => {
+    let query: QueryBuilder = database(TableNames.timeRecords)
+        .join(TableNames.milestones, 'timeRecords.milestoneId', '=', 'milestones.id')
+        .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
+        .join(TableNames.users, 'users.id', '=', 'timeRecords.userId')
+        .select(database.raw('concat(users.firstName, " ", users.lastName) as userName'), 'projects.name as projectName')
+        .groupBy('users.id', 'projects.id', 'projectName', 'userName')
+        .sum(database.raw('users.costToCompanyPerHour * timeRecords.spentTime'));
+    const report = await query;
+    console.log(report);
+    //res.json(report);
+    res.json(transformReportForFrontendGeneric(report, 'userName', 'projectName', 'sum(users.costToCompanyPerHour * timeRecords.spentTime)'));
+}
+
+export const generateReportUserByHours = async (req: Request, res: Response) => {
+    let query: QueryBuilder = database(TableNames.timeRecords)
+        .join(TableNames.milestones, 'timeRecords.milestoneId', '=', 'milestones.id')
+        .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
+        .join(TableNames.users, 'users.id', '=', 'timeRecords.userId')
+        .select('projects.name as projectName', database.raw('concat(users.firstName, " ", users.lastName) as userName'), 'projects.id as projectId', 'users.id as userId')
+        .groupBy('users.id', 'projects.id', 'projectName', 'userName')
+        .sum('timeRecords.spentTime as timeSpent');
+        const report = await query;
+        console.log(report);
+    res.json(transformReportForFrontendGeneric(report, 'projectName', 'userName', 'timeSpent'));
+}
+
+
+export const generateReportUserByPounds = async (req: Request, res: Response) => {
+    let query: QueryBuilder = database(TableNames.timeRecords)
+        .join(TableNames.milestones, 'timeRecords.milestoneId', '=', 'milestones.id')
+        .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
+        .join(TableNames.users, 'users.id', '=', 'timeRecords.userId')
+        .select('projects.name as projectName', database.raw('concat(users.firstName, " ", users.lastName) as userName'), 'projects.id as projectId', 'users.id as userId')
+        .groupBy('users.id', 'projects.id', 'projectName', 'userName')
+        .sum(database.raw('timeRecords.spentTime * users.costToCompanyPerHour'));
+    const report = await query;
+    //res.json(report);
+    res.json(transformReportForFrontendGeneric(report, 'projectName', 'userName', 'sum(timeRecords.spentTime * users.costToCompanyPerHour)'));
+}
+
+export const transformReportForFrontendGeneric = (report: Array<object>, columnName: string, rowName: string, data: string): object => {
+    let columnNames = getUniqueRowNames(report, rowName);
     let finalObject = {};
     columnNames.forEach(column => finalObject[column] = (new Object()));
     Object.keys(finalObject).forEach(key => {
@@ -29,11 +70,12 @@ export const parseReportForFrontendGeneric = (report: Array<object>, columnName:
     return finalObject;
 }
 
-const getUniqueColumnNames = (report): Array<any> => {
-    let uniqueColumnNames = new Set();
-    report.forEach(x => uniqueColumnNames.add(x.projectName));
-    return Array.from(uniqueColumnNames);
+const getUniqueRowNames = (report, rowName): Array<any> => {
+    let uniqueRowNames = new Set();
+    report.forEach(x => uniqueRowNames.add(x[rowName]));
+    return Array.from(uniqueRowNames);
 }
+
 export const calculateAndAddTotal = (finalObject: object) => {
     Object.keys(finalObject).forEach(element => {
         let sum = 0;
--- project-tracker backend/src/app/routers/report.ts
+++ project-tracker backend/src/app/routers/report.ts
@@ -3,4 +3,7 @@ import * as reportController from '../controllers/report';
 
 export const router: Router = Router({mergeParams: true});
 
-router.get('/', reportController.generateReportProjectByHours);
+router.get('/project/hours', reportController.generateReportProjectByHours);
+router.get('/project/pounds', reportController.generateReportProjectByPounds);
+router.get('/user/hours', reportController.generateReportUserByHours);
+router.get('/user/pounds', reportController.generateReportUserByPounds);
