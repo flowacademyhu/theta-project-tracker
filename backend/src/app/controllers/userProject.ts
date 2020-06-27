a88bb46ae575b84db8875486c4ae81221dc8079c
JJazon125 <jazon97@gmail.com>
2020-06-27 11:15:13 +0200

    404 for userProject

--- backend/src/app/controllers/userProject.ts
+++ backend/src/app/controllers/userProject.ts
@@ -11,15 +11,20 @@ export const index = async (req: Request, res: Response) => {
   } else {
     userId = +req.params.userId;
   }
-  let query: QueryBuilder = database(TableNames.projects)
-    .join(TableNames.projectUsers, 'projects.id', '=', 'projectUsers.projectId')
-    .where({userId: userId}).whereNull('projects.deletedAt').select();
-  if (req.query.limit) {
-    query = query.limit(req.query.limit);
+  const user= await database(TableNames.users).select().where({id: userId}).whereNull('deletedAt');
+  if(user) {
+    let query: QueryBuilder = database(TableNames.projects)
+      .join(TableNames.projectUsers, 'projects.id', '=', 'projectUsers.projectId')
+      .where({userId: userId}).whereNull('projects.deletedAt').select();
+    if (req.query.limit) {
+      query = query.limit(req.query.limit);
+    }
+    if (req.query.offset) {
+      query = query.offset(req.query.offset);
+    }
+    const projects: Array<Project> = await query;
+    res.status(200).json(projects);
+  } else {
+    res.sendStatus(404);
   }
-  if (req.query.offset) {
-    query = query.offset(req.query.offset);
-  }
-  const projects: Array<Project> = await query;
-  res.status(200).json(projects);
 }
