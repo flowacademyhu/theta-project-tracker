0d75d0537c5d9b5f0d73d334f3ee2d8a02efbc6e
MasaT91 <tamas.szemesi@gmail.com>
2020-06-29 14:17:24 +0200

    added internationalization

--- frontend/theta-project-tracker/src/app/modals/confirm-modal.ts
+++ frontend/theta-project-tracker/src/app/modals/confirm-modal.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
     <mat-dialog-content class="mat-typography">
     </mat-dialog-content>
     <mat-dialog-actions class="actions">
-      <button mat-raised-button mat-dialog-close class="left" color="accent">No</button>
-      <button mat-raised-button [mat-dialog-close]="true" class="right" color="warn" >Yes</button>
+      <button mat-raised-button mat-dialog-close class="left" color="accent">{{ 'no' | translate }}</button>
+      <button mat-raised-button [mat-dialog-close]="true" class="right" color="warn" >{{ 'yes' | translate }}</button>
     </mat-dialog-actions>
   `,
   styles: [`
