4abc4be90d5deec5d647f1fb131fe9a084e1a057
Youngcatlady95 <szizitaa@gmail.com>
2020-06-30 16:10:36 +0200

    minor css magic on user's table

--- frontend/theta-project-tracker/src/app/components/users.component.ts
+++ frontend/theta-project-tracker/src/app/components/users.component.ts
@@ -8,7 +8,6 @@ import { NewUserModalComponent } from '../modals/new-user-modal.component';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { ProjectUsersService } from '../services/projectUsers.service';
-import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-users',
@@ -17,14 +16,10 @@ import { Router, ActivatedRoute } from '@angular/router';
     <div>
     <button (click)="onAddNewUser()" mat-raised-button>{{'add-user' | translate}}</button>
         <mat-table [dataSource]="users" class="mat-elevation-z8">
-            <ng-container matColumnDef="firstName">
-                <mat-header-cell *matHeaderCellDef>{{ 'firstname' | translate}}</mat-header-cell>
-                <mat-cell *matCellDef="let user">{{ user.firstName }}</mat-cell>
+            <ng-container matColumnDef="name">
+                <mat-header-cell *matHeaderCellDef>{{ 'name' | translate}}</mat-header-cell>
+                <mat-cell *matCellDef="let user">{{ user.fullName }}</mat-cell>
             </ng-container>
-            <ng-container matColumnDef="lastName">
-            <mat-header-cell *matHeaderCellDef>{{'lastname' | translate}}</mat-header-cell>
-            <mat-cell *matCellDef="let user">{{ user.lastName }}</mat-cell>
-        </ng-container>
             <ng-container matColumnDef="role">
                 <mat-header-cell *matHeaderCellDef>{{'role' | translate}}</mat-header-cell>
                 <mat-cell *matCellDef="let user">{{ user.role }}</mat-cell>
@@ -35,7 +30,7 @@ import { Router, ActivatedRoute } from '@angular/router';
             </ng-container>
             <ng-container matColumnDef="projects" >
                 <mat-header-cell *matHeaderCellDef>{{'projects' | translate}}</mat-header-cell>
-                <mat-cell *matCellDef="let user"><p *ngFor="let project of user.projects">{{ project.name }}</p></mat-cell>
+                <mat-cell *matCellDef="let user"><p *ngFor="let project of user.projects">{{ project.name }}&emsp;</p></mat-cell>
             </ng-container>
             <ng-container matColumnDef="actions" class="actions">
                 <mat-header-cell *matHeaderCellDef>{{'actions' | translate}}</mat-header-cell>
@@ -59,21 +54,36 @@ import { Router, ActivatedRoute } from '@angular/router';
     `
     .table-container {
       margin: auto;
-      max-width: 70%;
-      height: 450px;
+      max-width: 65%;
+      height: 100%;
       overflow: auto;
-      margin-top: 200px
+      margin-top: 100px;
+      padding: 3rem
     }
     mat-icon:hover {
       cursor: pointer;
     }
+    .mat-column-projects {
+      flex: 0 0 35%;
+    }
+    .mat-column-name {
+      flex: 0 0 20%;
+    }
+    .mat-column-role {
+      flex: 0 0 15%;
+    }
+    .mat-column-cost {
+      flex: 0 0 15%;
+    }
+ 
+
     `]
 })
 export class UsersComponent implements OnInit, OnDestroy, AfterViewInit {
 
   subscriptions$: Subscription[] = [];
   projectArrays: any[] = [];
-  displayedColumns = ['firstName', 'lastName', 'role', 'cost', 'projects', 'actions'];
+  displayedColumns = ['name', 'role', 'cost', 'projects', 'actions'];
   users: User[] = [];
   dataSource: MatTableDataSource<User> = new MatTableDataSource<User>();
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -89,6 +99,7 @@ export class UsersComponent implements OnInit, OnDestroy, AfterViewInit {
           u.projects = array
         })
       })
+      this.users.map(u => u.fullName = u.firstName + ' ' + u.lastName)
     });
   }
 
@@ -105,7 +116,6 @@ export class UsersComponent implements OnInit, OnDestroy, AfterViewInit {
       height: '25%'
     });
     this.subscriptions$.push(dialogRef.afterClosed().subscribe(result => {
-      console.log(user.id)
       if (result) {
         this.userService.deleteUser(user.id).subscribe(() => {
           this.updateDataSource();
@@ -141,7 +151,8 @@ export class UsersComponent implements OnInit, OnDestroy, AfterViewInit {
       this.users = users;
       this.users.map(u => {
         this.projectUserService.getUsersProjects(u.id).subscribe(array => {
-          u.projects = array
+          u.projects = array;
+          u.fullName = u.firstName + ' ' + u.lastName
         })
       })
     });
