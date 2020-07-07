6aecb1d5f5b2706c13e6d09734158024b46772d2
JJazon125 <jazon97@gmail.com>
2020-07-07 20:33:12 +0200

    refact

--- backend/src/app/controllers/timeRecordCopy.ts
+++ backend/src/app/controllers/timeRecordCopy.ts
@@ -10,6 +10,33 @@ import * as userTimeRecordSerializer from "../serializers/userTimeRecord";
 import {TimeRecord} from "../models/timeRecord";
 import * as _ from "lodash";
 
+const pastArray = async (res: Response, fromDatePrevious) => {
+  const pastWeekQuery: QueryBuilder = database(TableNames.userTimeRecords)
+    .where('week', '=', fromDatePrevious)
+    .where({userId: res.locals.user.id})
+    .select();
+  return pastWeekQuery;
+}
+
+const currentArray = async (res: Response, fromDate) => {
+   const currentWeekQuery: QueryBuilder = database(TableNames.userTimeRecords)
+    .where('week', '=', fromDate)
+    .where({userId: res.locals.user.id})
+    .select();
+   return currentWeekQuery;
+}
+
+const compareAndCreate = async (pastWeekArray, currentWeekArray, fromDate) => {
+  const diff = _.differenceBy(pastWeekArray, currentWeekArray, 'milestoneId', 'actionLabelId');
+  let newUserTimeRecords: Array<UserTimeRecord> = userTimeRecordSerializer.copy(diff, fromDate);
+  let newUserTimeRecordIds: Array<number> = [];
+  for (let item of newUserTimeRecords) {
+    newUserTimeRecordIds.push(await database(TableNames.userTimeRecords).insert(item));
+  }
+  let newTimeRecords: Array<TimeRecord> = timeRecordSerializer.copy(newUserTimeRecordIds, pastWeekArray, fromDate);
+  await database(TableNames.timeRecords).insert(newTimeRecords);
+}
+
 export const create = async (req: Request, res: Response) => {
   try {
     let date: Moment;
@@ -20,25 +47,10 @@ export const create = async (req: Request, res: Response) => {
     }
     const fromDate = date.startOf('isoWeek').format('YYYY-MM-DD');
     const fromDatePrevious = moment(fromDate).subtract(1, 'week').format('YYYY-MM-DD');
-    const pastWeekQuery: QueryBuilder = database(TableNames.userTimeRecords)
-      .where('week', '=', fromDatePrevious)
-      .where({userId: res.locals.user.id})
-      .select();
-    const currentWeekQuery: QueryBuilder = database(TableNames.userTimeRecords)
-      .where('week', '=', fromDate)
-      .where({userId: res.locals.user.id})
-      .select();
-    const pastWeekArray: Array<any> = await pastWeekQuery;
-    const currentWeekArray = await currentWeekQuery;
-    const diff = _.differenceBy(pastWeekArray, currentWeekArray, 'milestoneId', 'actionLabelId');
-    let newUserTimeRecords: Array<UserTimeRecord> = userTimeRecordSerializer.copy(diff, fromDate);
-    let newUserTimeRecordIds: Array<number> = [];
-    for (let item of newUserTimeRecords) {
-      newUserTimeRecordIds.push(await database(TableNames.userTimeRecords).insert(item));
-    }
-    let newTimeRecords: Array<TimeRecord> = timeRecordSerializer.copy(newUserTimeRecordIds, pastWeekArray, fromDate);
-    await database(TableNames.timeRecords).insert(newTimeRecords);
-    res.status(201);
+    const pastWeekArray = await pastArray(res, fromDatePrevious);
+    const currentWeekArray = await currentArray(res, fromDate);
+    await compareAndCreate(pastWeekArray, currentWeekArray, fromDate);
+    res.sendStatus(201);
   } catch
     (error) {
     console.error(error);
