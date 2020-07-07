c1c70916515bad8c55f69eebb8f5461ea4738f41
viktor <szabomatedenes@gmail.com>
2020-07-07 15:55:17 +0200

    sending file and 200 response

--- backend/src/app/controllers/export.ts
+++ backend/src/app/controllers/export.ts
@@ -6,29 +6,34 @@ import * as reportQuery from "../../lib/reportQuery"
 export const exportReportProjectByHours = async (req: Request, res) => {
     const report = await reportQuery.queryReportProjectByHours();
     createExcelReport('Report per project', reportSerializer.getReportProjectByHours(report));
-    res.status(200).sendExcelFile(res);
+    sendExcelFile(res);
+    res.status(200);
 }
 
 export const exportReportProjectByCost = async (req: Request, res) => {
     const report = await reportQuery.queryReportProjectByCost();
     createExcelReport('Report per project', reportSerializer.getReportProjectByCost(report));
     sendExcelFile(res);
+    res.status(200);
 }
 
 export const exportReportUserByHours = async (req: Request, res) => {
     const report = await reportQuery.queryReportUserByHours();
     createExcelReport('Report per contractor', reportSerializer.getReportUserByHours(report));
     sendExcelFile(res);
+    res.status(200);
 }
 
 export const exportReportUserByCost = async (req: Request, res) => {
     const report = await reportQuery.queryReportUserByCost();
     createExcelReport('Report per contractor', reportSerializer.getReportUserByCost(report));
     sendExcelFile(res);
+    res.status(200);
 }
 
 export const exportReportBudget = async (req: Request, res) => {
     let data = await reportQuery.queryReportBudget();
     createExcelReport('Project Profitability', reportSerializer.getBudgetReport(data));
     sendExcelFile(res);
+    res.status(200);
 }
--- backend/src/lib/reportQuery.ts
+++ backend/src/lib/reportQuery.ts
@@ -1,16 +1,16 @@
 import { QueryBuilder } from "knex";
 import { database } from "./database";
-import { Request, Response } from "express";
 import { TableNames } from "./enums";
 
 export const queryReportProjectByHours = async () => {
     let query: QueryBuilder = database(TableNames.timeRecords)
-        .join(TableNames.milestones, 'timeRecords.milestoneId', '=', 'milestones.id')
+        .join(TableNames.userTimeRecords, 'timeRecords.userTimeRecordId', '=', 'userTimeRecords.id')
+        .join(TableNames.users, 'userTimeRecords.id', '=', 'userId')
+        .join(TableNames.milestones, 'userTimeRecords.milestoneId', '=', 'milestones.id')
         .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
-        .join(TableNames.users, 'users.id', '=', 'timeRecords.userId')
         .select('projects.name as projectName', database.raw('concat(users.firstName, " ", users.lastName) as userName'),
-        database.raw('sum(timeRecords.spentTime + timeRecords.overTime) as timeSpent'),
-        'projects.id as projectId', 'users.id as userId')
+            database.raw('sum(timeRecords.normalHours + timeRecords.overTime) as timeSpent'),
+            'projects.id as projectId', 'users.id as userId')
         .groupBy('users.id', 'projects.id', 'projectName', 'userName')
     const report = await query;
     return(report);  
@@ -18,13 +18,14 @@ export const queryReportProjectByHours = async () => {
 
 export const queryReportProjectByCost = async () => {
     let query: QueryBuilder = database(TableNames.timeRecords)
-        .join(TableNames.milestones, 'timeRecords.milestoneId', '=', 'milestones.id')
-        .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
-        .join(TableNames.users, 'users.id', '=', 'timeRecords.userId')
-        .select(database.raw('concat(users.firstName, " ", users.lastName) as userName'), 'projects.name as projectName',
-        database.raw('sum(users.costToCompanyPerHour * (timeRecords.spentTime + timeRecords.overTime)) as cost'))
-        .groupBy('users.id', 'projects.id', 'projectName', 'userName')
-    const report = await query;
+    .join(TableNames.userTimeRecords, 'timeRecords.userTimeRecordId', '=', 'userTimeRecords.id')
+    .join(TableNames.users, 'userTimeRecords.id', '=', 'userId')
+    .join(TableNames.milestones, 'userTimeRecords.milestoneId', '=', 'milestones.id')
+    .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
+    .select(database.raw('concat(users.firstName, " ", users.lastName) as userName'), 'projects.name as projectName',
+        database.raw('sum(users.costToCompanyPerHour * (timeRecords.normalHours + timeRecords.overTime)) as cost'))
+    .groupBy('users.id', 'projects.id', 'projectName', 'userName')
+const report = await query;
     return(report);  
 }
 
@@ -42,11 +43,12 @@ export const queryReportUserByHours = async () => {
 
 export const queryReportUserByCost = async () => {
     let query: QueryBuilder = database(TableNames.timeRecords)
-        .join(TableNames.milestones, 'timeRecords.milestoneId', '=', 'milestones.id')
+        .join(TableNames.userTimeRecords, 'timeRecords.userTimeRecordId', '=', 'userTimeRecords.id')
+        .join(TableNames.users, 'userTimeRecords.id', '=', 'userId')
+        .join(TableNames.milestones, 'userTimeRecords.milestoneId', '=', 'milestones.id')
         .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
-        .join(TableNames.users, 'users.id', '=', 'timeRecords.userId')
         .select('projects.name as projectName', database.raw('concat(users.firstName, " ", users.lastName) as userName'),
-        'projects.id as projectId', 'users.id as userId', database.raw('sum(users.costToCompanyPerHour * (timeRecords.spentTime + timeRecords.overTime)) as cost'))
+            'projects.id as projectId', 'users.id as userId', database.raw('sum(users.costToCompanyPerHour * (timeRecords.normalHours + timeRecords.overTime)) as cost'))
         .groupBy('users.id', 'projects.id', 'projectName', 'userName')
     const report = await query;
     return(report);  
@@ -54,14 +56,15 @@ export const queryReportUserByCost = async () => {
 
 export const queryReportBudget = async () => {
     let query: QueryBuilder = database(TableNames.timeRecords)
-    .join(TableNames.milestones, 'timeRecords.milestoneId', '=', 'milestones.id')
-    .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
-    .join(TableNames.users, 'users.id', '=', 'timeRecords.userId')
-    .select('projects.name as projectName')
-    .select(database.raw('sum(users.costToCompanyPerHour * (timeRecords.spentTime + timeRecords.overTime)) as "actualCosts"'))
-    .select('projects.budget as budgetCosts')
-    .select(database.raw('projects.budget -(sum(users.costToCompanyPerHour * (timeRecords.spentTime + timeRecords.overTime))) as "overUnder"'))
-    .groupBy('projects.name', 'projects.budget')
+        .join(TableNames.userTimeRecords, 'timeRecords.userTimeRecordId', '=', 'userTimeRecords.id')
+        .join(TableNames.users, 'userTimeRecords.id', '=', 'userId')
+        .join(TableNames.milestones, 'userTimeRecords.milestoneId', '=', 'milestones.id')
+        .join(TableNames.projects, 'milestones.projectId', '=', 'projects.id')
+        .select('projects.name as projectName')
+        .select(database.raw('sum(users.costToCompanyPerHour * (timeRecords.normalHours + timeRecords.overTime)) as "actualCosts"'))
+        .select('projects.budget as budgetCosts')
+        .select(database.raw('projects.budget -(sum(users.costToCompanyPerHour * (timeRecords.normalHours + timeRecords.overTime))) as "overUnder"'))
+        .groupBy('projects.name', 'projects.budget')
     const report = await query;
     return(report);  
 }
