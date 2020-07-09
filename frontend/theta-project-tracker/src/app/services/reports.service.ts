6474983e8371ee80bc57fcd3be2f92f1676f6b9b
mate3241 <61408923+mate3241@users.noreply.github.com>
2020-07-09 12:29:59 +0200

    I194 export filter (#198)
    * filter of projects/users on FE
    
    * typo
    
    * no backend
    
    * rewriting
    
    * subscribe
    
    * bugfix
    
    * -subscribe

--- frontend/theta-project-tracker/src/app/services/reports.service.ts
+++ frontend/theta-project-tracker/src/app/services/reports.service.ts
@@ -15,38 +15,54 @@ export class ReportsService {
 
   constructor(private http: HttpClient) { }
   apiUrl = environment.baseUrl;
-  getReportsByProjectHours(startDate, endDate) {
+  getReportsByProjectHours(startDate, endDate, projects) {
+    const arrayString = JSON.stringify(this.createArray(projects));
     return this.http.get(this.apiUrl + 'report/project/hours',{ params: {
       from: startDate,
-      to: endDate
+      to: endDate,
+      projects: arrayString
     }} );
   }
 
-  getReportsByProjectCost(startDate, endDate): Observable<Result> {
+  getReportsByProjectCost(startDate, endDate, projects): Observable<Result> {
+    const arrayString = JSON.stringify(this.createArray(projects));
     return this.http.get<Result>(this.apiUrl + 'report/project/cost',{ params: {
       from: startDate,
-      to: endDate
+      to: endDate,
+      projects: arrayString
     }} );
   }
 
-  getReportsByUserHours(startDate, endDate): Observable<Result> {
+  getReportsByUserHours(startDate, endDate, users): Observable<Result> {
+    const arrayString = JSON.stringify(this.createArray(users));
     return this.http.get<Result>(this.apiUrl + 'report/user/hours',{ params: {
       from: startDate,
-      to: endDate
+      to: endDate,
+      users: arrayString
     }} );
   }
 
-  getReportsByUserCost(startDate, endDate): Observable<Result> {
+  getReportsByUserCost(startDate, endDate, users): Observable<Result> {
+    const arrayString = JSON.stringify(this.createArray(users));
     return this.http.get<Result>(this.apiUrl + 'report/user/cost',{ params: {
       from: startDate,
-      to: endDate
+      to: endDate,
+      users: arrayString
     }} );
   }
 
-  getReportsBudget(startDate, endDate): Observable<any> {
+  getReportsBudget(startDate, endDate, projects): Observable<any> {
+    const arrayString = JSON.stringify(this.createArray(projects));
     return this.http.get<any>(this.apiUrl + 'report/project/budget',{ params: {
       from: startDate,
       to: endDate
     }} );
+  }
+  createArray(original) {
+    const result = [];
+    original.forEach(value => {
+      result.push(value.id);
+    })
+    return result;
   }
 }
