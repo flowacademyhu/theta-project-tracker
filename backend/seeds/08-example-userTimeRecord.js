90d02e267e56a73871f30fa30b1ba4f847ad5948
JJazon125 <jazon97@gmail.com>
2020-07-09 21:40:58 +0200

    seed

--- backend/seeds/08-example-userTimeRecord.js
+++ backend/seeds/08-example-userTimeRecord.js
@@ -13,6 +13,20 @@ exports.seed = (knex) => {
             actionLabelId: 4,
             description: "postman testing",
             week: '2020-06-15'
+        },
+        {
+            userId: 3,
+            milestoneId: 1,
+            actionLabelId: 1,
+            description: "header",
+            week: '2020-06-15'
+        },
+        {
+            userId: 4,
+            milestoneId: 1,
+            actionLabelId: 1,
+            description: "CRUD",
+            week: '2020-06-15'
         }
     ])
 }
--- backend/seeds/09-example-timeRecords.js
+++ backend/seeds/09-example-timeRecords.js
@@ -84,5 +84,89 @@ exports.seed = (knex) => {
             overTime: 0,
             date: '2020-06-21'
         },
+        {
+            userTimeRecordId: 3,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-15'
+        },
+        {
+            userTimeRecordId: 3,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-16'
+        },
+        {
+            userTimeRecordId: 3,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-17'
+        },
+        {
+            userTimeRecordId: 3,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-18'
+        },
+        {
+            userTimeRecordId: 3,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-19'
+        },
+        {
+            userTimeRecordId: 3,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-20'
+        },
+        {
+            userTimeRecordId: 3,
+            normalHours: 0,
+            overTime: 0,
+            date: '2020-06-21'
+        },
+        {
+            userTimeRecordId: 4,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-15'
+        },
+        {
+            userTimeRecordId: 4,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-16'
+        },
+        {
+            userTimeRecordId: 4,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-17'
+        },
+        {
+            userTimeRecordId: 4,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-18'
+        },
+        {
+            userTimeRecordId: 4,
+            normalHours: 8,
+            overTime: 0,
+            date: '2020-06-19'
+        },
+        {
+            userTimeRecordId: 4,
+            normalHours: 0,
+            overTime: 6,
+            date: '2020-06-20'
+        },
+        {
+            userTimeRecordId: 4,
+            normalHours: 0,
+            overTime: 2,
+            date: '2020-06-21'
+        }
     ])
 }
