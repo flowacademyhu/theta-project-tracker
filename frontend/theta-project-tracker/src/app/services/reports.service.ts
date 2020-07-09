664df7ea088374d5e338ba2f8310de044d6e4b9a
Tamas Koszo <tamas.koszo@voodoopark.com>
2020-07-09 22:55:13 +0200

    export and report logic polish
    - clean up logic duplications during export, add the ability to download XLSX
    - adjust report logic and component

--- frontend/theta-project-tracker/src/app/services/reports.service.ts
+++ frontend/theta-project-tracker/src/app/services/reports.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
 
 export interface Result {
   [projectName: string]: {
     [userName: string]: number;
     total: number;
-  }
+  };
 }
 
 export enum ReportRoute {
-  BY_PROJECT_HOURS = 'report/project/hours',
-  BY_PROJECT_COST = 'report/project/cost',
-  BY_USER_HOURS = 'report/user/hours',
-  BY_USER_COST = 'report/user/cost',
-  BY_BUDGET = 'report/project/budget'
+  BY_PROJECT_HOURS = '/project/hours',
+  BY_PROJECT_COST = '/project/cost',
+  BY_USER_HOURS = '/user/hours',
+  BY_USER_COST = '/user/cost',
+  BY_BUDGET = '/project/budget'
 }
 
 @Injectable({providedIn: 'root'})
@@ -24,7 +23,7 @@ export class ReportsService {
 
   constructor(private http: HttpClient) { }
 
-  getReports(start, end, array: any[], route: ReportRoute) {
+  getReports(start, end, array: any[], route: ReportRoute, isUser: boolean) {
     let params: HttpParams = new HttpParams();
     params = params.append('from', start);
     params = params.append('to', end);
@@ -32,6 +31,6 @@ export class ReportsService {
       params = params.append('projects', JSON.stringify(array.map(i => i.id)));
     }
 
-    return this.http.get(this.apiUrl + route, { params });
+    return this.http.get(this.apiUrl + 'report' + route, { params });
   }
 }
