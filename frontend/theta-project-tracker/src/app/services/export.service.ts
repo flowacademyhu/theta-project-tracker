4d49cd4fc8895e3490413114c14ae110694e0dcf
viktor <szabomatedenes@gmail.com>
2020-07-09 11:18:11 +0200

    subscribe

--- frontend/theta-project-tracker/src/app/services/export.service.ts
+++ frontend/theta-project-tracker/src/app/services/export.service.ts
@@ -31,7 +31,7 @@ export class ExportsService {
       from: startDate,
       to: endDate,
       users: arrayString
-    }});
+    }}).subscribe();
   }
 
   exportReportsByUserCost(startDate, endDate, users) {
@@ -40,7 +40,7 @@ export class ExportsService {
       from: startDate,
       to: endDate,
       users: arrayString
-    }});
+    }}).subscribe();
   }
 
   exportReportsBudget(startDate, endDate, projects) {
@@ -49,7 +49,7 @@ export class ExportsService {
       from: startDate,
       to: endDate,
       projects: arrayString
-    }});
+    }}).subscribe();
   }
 
   createArray(original) {
